ca156a92e862cf8dc5fd0fcc82e388d6bd66a05b
Rana Umair <rjee4631@gmail.com>
2022-05-12 18:00:19 +0500

    Update clicktoCopyToClipBoard______with_js____.js

--- clicktoCopyToClipBoard______with_js____.js
+++ clicktoCopyToClipBoard______with_js____.js
@@ -5,3 +5,13 @@ function copyToClipboard(element) {
   document.execCommand("copy");
   $temp.remove();
 }
+
+
+function copyToClipboard(text) {
+   const elem = document.createElement('textarea');
+   elem.value = text;
+   document.body.appendChild(elem);
+   elem.select();
+   document.execCommand('copy');
+   document.body.removeChild(elem);
+}
